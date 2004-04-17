8700464c2ab0603608d6ecebb4ac8ff81f304377
ralbertoni <ralbertoni>
2004-04-17 14:46:50 +0000

    Source changed

--- arconnor/WEB-INF/classes/it/bma/comuni/BmaException.java
+++ arconnor/WEB-INF/classes/it/bma/comuni/BmaException.java
@@ -2,77 +2,74 @@ package it.bma.comuni;
 
 public class BmaException extends Exception
 													implements java.io.Serializable, 
-													BmaLiterals {
+													BmaLiterals, BmaErrorCodes {
 	private BmaErrore error = new BmaErrore();
-public BmaException() {
-	super();
-}
-public BmaException(String s) {
-	super(s);
-	error.setInfo(s);
-	error.setInfoEstese(s);
-}
-public BmaException(String info, String infoEstese) {
-	super(info);
-	error.setInfo(info);
-	error.setInfoEstese(infoEstese);
-}
-public BmaException(String codErrore, String info, String infoEstese, String nomeOggetto) {
-	getInstance(codErrore, info, infoEstese, nomeOggetto);
-}
-public BmaException(String codErrore, String info, String infoEstese, BmaObject obj) {
-	getInstance(codErrore, info, infoEstese, obj.getClass().getName());
-}
-private BmaException getInstance(String codErrore, String info, String infoEstese, String nomeOggetto) {
-	BmaException instance = new BmaException(info);
-	BmaErrori list = BmaErrori.getInstance();
-	if (list == null) {
-		instance.error = new BmaErrore();
-		instance.error.setCodErrore(error.BMA_ERR_CONFIGURAZIONE);
-		instance.error.setTipo(error.BMA_ERR_ABEND);
-		instance.error.setMsgUtente(error.BMA_MSG_LISTA_ERRORI_VUOTA);
-		instance.error.setMsgSistema(error.BMA_MSG_LISTA_ERRORI_VUOTA);
+	public BmaException() {
+		super();
+		impostaErrore(BMA_ERR_NON_PREVISTO, "", "", "");
 	}
-	else {
-		instance.error = list.getErrore(codErrore);
-		if (error == null) {
-			instance.error = new BmaErrore();
-			instance.error.setCodErrore(error.BMA_ERR_NON_PREVISTO);
-			instance.error.setTipo(error.BMA_ERR_ABEND);
-			instance.error.setMsgUtente(error.BMA_MSG_ERRORE_NON_PREVISTO);
-			instance.error.setMsgSistema(error.BMA_MSG_ERRORE_NON_PREVISTO + ": " + codErrore);
+	public BmaException(String info) {
+		super(info);
+		impostaErrore(BMA_ERR_NON_PREVISTO, info, info, "");
+	}
+	public BmaException(String info, String infoEstese) {
+		super(info);
+		impostaErrore(BMA_ERR_NON_PREVISTO, info, infoEstese, "");
+	}
+	public BmaException(String codErrore, String info, String infoEstese, String nomeOggetto) {
+		super(info);
+		impostaErrore(codErrore, info, infoEstese, nomeOggetto);
+	}
+	public BmaException(String codErrore, String info, String infoEstese, BmaObject obj) {
+		super(info);
+		impostaErrore(codErrore, info, infoEstese, obj.getClass().getName());
+	}
+	private void impostaErrore(String codErrore, String info, String infoEstese, String nomeOggetto) {
+		BmaErrori list = BmaErrori.getInstance();
+		if (list == null) {
+			error.setCodErrore(error.BMA_ERR_CONFIGURAZIONE);
+			error.setTipo(error.BMA_ERR_ABEND);
+			error.setMsgUtente(error.BMA_MSG_LISTA_ERRORI_VUOTA);
+			error.setMsgSistema(error.BMA_MSG_LISTA_ERRORI_VUOTA);
 		}
+		else {
+			error = list.getErrore(codErrore);
+			if (error == null) {
+				error.setCodErrore(error.BMA_ERR_NON_PREVISTO);
+				error.setTipo(error.BMA_ERR_ABEND);
+				error.setMsgUtente(error.BMA_MSG_ERRORE_NON_PREVISTO);
+				error.setMsgSistema(error.BMA_MSG_ERRORE_NON_PREVISTO + ": " + codErrore);
+			}
+		}
+		error.setInfo(info);
+		error.setInfoEstese(infoEstese);
+		error.setNomeOggetto(nomeOggetto);
 	}
-	instance.error.setInfo(info);
-	instance.error.setInfoEstese(infoEstese);
-	instance.error.setNomeOggetto(nomeOggetto);
-	return instance;
-}
-public BmaErrore getErrore() {
-	return error;
-}
-public String getInfo() {
-	if (error == null) return "";
-	else return error.getInfo();
-}
-public String getInfoEstese() {
-	if (error==null) return "";
-	else return error.getInfoEstese();
-}
-public String getNomeOggetto() {
-	if (error == null) return "";
-	else return error.getNomeOggetto();
-}
-public void setErrore(BmaErrore newErrore) {
-	error = newErrore;
-}
-public void setInfo(String info) {
-	error.setInfo(info);
-}
-public void setInfoEstese(String info) {
-	error.setInfoEstese(info);
-}
-public void setNomeOggetto(String obj) {
-	error.setNomeOggetto(obj);
-}
+	public BmaErrore getErrore() {
+		return error;
+	}
+	public String getInfo() {
+		if (error == null) return "";
+		else return error.getInfo();
+	}
+	public String getInfoEstese() {
+		if (error==null) return "";
+		else return error.getInfoEstese();
+	}
+	public String getNomeOggetto() {
+		if (error == null) return "";
+		else return error.getNomeOggetto();
+	}
+	public void setErrore(BmaErrore newErrore) {
+		error = newErrore;
+	}
+	public void setInfo(String info) {
+		error.setInfo(info);
+	}
+	public void setInfoEstese(String info) {
+		error.setInfoEstese(info);
+	}
+	public void setNomeOggetto(String obj) {
+		error.setNomeOggetto(obj);
+	}
 }
